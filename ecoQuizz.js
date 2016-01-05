a26dc430d48fe7e687d2e53c81428a6adf6521fe
Juliette Rivière <juliette@riviere.im>
2016-01-05 13:23:46 +0100

    gain/lose life

--- ecoQuizz.js
+++ ecoQuizz.js
@@ -7,28 +7,43 @@ $( function() {
 	setLife();
 	buttons.click( function() {
 		var section_suivante = $(this).attr("go");
+		var etat = $(this).attr("life")
+		updateLife(etat);
+		
 		$('.section:visible').fadeOut('200', function() {
 			gotoSection(section_suivante);
 		});
-	} );
+	});
 
 
 
 	$('.section').click(function() {
+			setLife();
 
 	});
+
+	function updateLife(etat) {
+		if ( etat == "win") {
+			gainOneLife();
+		} else {
+			loseOneLife();
+		};
+	}
 	
 	function gotoSection(key) {
 		var change_section = ('#' + key);
 		$(change_section).show();
-
 	}
 	
 	function getLife() {
-		//...
-
+	// ...
 	}
 	
+	function gainOneLife() {
+		life ++;	
+
+	}
+
 	function setLife(v) {
 		//...
 		$(".value").html(life);
