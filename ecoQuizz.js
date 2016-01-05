922403de9dc70177042f80ff06f945bbe9701b41
Juliette Rivière <juliette@riviere.im>
2016-01-05 14:08:14 +0100

    section die'

--- ecoQuizz.js
+++ ecoQuizz.js
@@ -1,25 +1,34 @@
 $( function() {
 	var buttons = $(".section button");
 	var status = $("#status");
-	var life = 10;
+	var life = 1;
 	
 	startGame();
 	setLife();
+
 	buttons.click( function() {
 		var section_suivante = $(this).attr("go");
-		var etat = $(this).attr("life")
+		var etat = $(this).attr("life");
+
+
 		updateLife(etat);
-		
+		getLife();
+
 		$('.section:visible').fadeOut('200', function() {
+		 if (life == 2) {
+	 	gotoSection("death");
+
+	 } else {
 			gotoSection(section_suivante);
+
+		};
 		});
 	});
 
 
 
 	$('.section').click(function() {
-			setLife();
-
+		setLife();
 	});
 
 	function updateLife(etat) {
@@ -36,7 +45,7 @@ $( function() {
 	}
 	
 	function getLife() {
-	// ...
+	
 	}
 	
 	function gainOneLife() {
@@ -45,7 +54,7 @@ $( function() {
 	}
 
 	function setLife(v) {
-		//...
+		
 		$(".value").html(life);
 
 	}
